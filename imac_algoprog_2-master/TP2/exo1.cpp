ab386b5309621bd003922fc89be713479140ded1
SégolèneLazarus <seg.laz@ik.me>
2023-02-26 16:39:48 +0100

    tp2 exo1&2

--- imac_algoprog_2-master/TP2/exo1.cpp
+++ imac_algoprog_2-master/TP2/exo1.cpp
@@ -1,21 +1,80 @@
-#include <QApplication>
+// #include <QApplication>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#include "tp2.h"
+#include <random>
+// #include "tp2.h"
 
-MainWindow* w = nullptr;
+const int tailleTb = 8;
 
-void selectionSort(Array& toSort){
-	// selectionSort
+
+
+// MainWindow* w = nullptr;
+
+// void selectionSort(int toSort[]){
+// 	for(int i=0;i<tailleTb;i++){
+//         for(int k=i;k<tailleTb;k++){
+//             if(toSort[i] > toSort[k]){
+//                 int n = toSort[i];
+//                 toSort[i] = toSort[k];
+//                 toSort[k] = n;
+//                 k=tailleTb;
+//             }
+//         }
+//     }
+// }
+
+void selectionSort(int toSort[]){
+    bool comtuveu = true;
+    while(comtuveu){
+        comtuveu = false;
+        for(int i=0;i<tailleTb;i++){
+            for(int k=i;k<tailleTb;k++){
+                if(toSort[i] > toSort[k]){
+                    int n = toSort[i];
+                    toSort[i] = toSort[k];
+                    toSort[k] = n;
+                    k=tailleTb;
+                    comtuveu = true;
+                }
+            }
+        }
+    }
+	
 }
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-    uint elementCount=15; // number of elements to sort
-    MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
-    w = new TestMainWindow(selectionSort); // window which display the behavior of the sort algorithm
-    w->show();
 
-	return a.exec();
+    srand((unsigned)time(NULL));
+	// QApplication a(argc, argv);
+    // uint elementCount=15; // number of elements to sort
+    // MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
+    // w = new TestMainWindow(selectionSort); // window which display the behavior of the sort algorithm
+    // w->show();
+
+	// return a.exec();
+
+    int tab[tailleTb];
+ 
+
+    for(int i=0;i<tailleTb;i++){
+        tab[i]=rand()%10;
+    }
+
+    for(int i=0;i<tailleTb;i++){
+        std::cout << tab[i] << "\n";
+    }
+
+    selectionSort(tab);
+
+    std::cout << "hahahahaha" << "\n";
+
+    for(int i=0;i<tailleTb;i++){
+        std::cout << tab[i] << "\n";
+    }
+
+    
 }
--- imac_algoprog_2-master/TP2/exo2.cpp
+++ imac_algoprog_2-master/TP2/exo2.cpp
@@ -1,25 +1,56 @@
-#include <QApplication>
+// #include <QApplication>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#include "tp2.h"
+#include <random>
+// #include "tp2.h"
 
-MainWindow* w=nullptr;
+// MainWindow* w=nullptr;
+void insertionSort(int toSort[]){
+	// Array& sorted=w->newArray(toSort.size());
 
-void insertionSort(Array& toSort){
-	Array& sorted=w->newArray(toSort.size());
-
-	// insertion sort from toSort to sorted
+	// // insertion sort from toSort to sorted
 	
-	toSort=sorted; // update the original array
+	// toSort=sorted; // update the original array
 }
+
+void insertionSort(int toSort[]){
+	
+}
+
+// int main(int argc, char *argv[])
+// {
+// 	QApplication a(argc, argv);
+//     uint elementCount=15; // number of elements to sort
+//     MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
+//     w = new TestMainWindow(insertionSort); // window which display the behavior of the sort algorithm
+// 	w->show();
+
+// 	return a.exec();
+// }
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-    uint elementCount=15; // number of elements to sort
-    MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
-    w = new TestMainWindow(insertionSort); // window which display the behavior of the sort algorithm
-	w->show();
 
-	return a.exec();
+	srand((unsigned)time(NULL));
+	int tab[tailleTb];
+ 
+
+    for(int i=0;i<tailleTb;i++){
+        tab[i]=rand()%10;
+    }
+
+    for(int i=0;i<tailleTb;i++){
+        std::cout << tab[i] << "\n";
+    }
+
+    insertionSort(tab);
+
+    std::cout << "hahahahaha" << "\n";
+
+    for(int i=0;i<tailleTb;i++){
+        std::cout << tab[i] << "\n";
+    }
 }
