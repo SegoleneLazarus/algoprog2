9774ace8e0d508d9a4284597dd8b5a327b85e876
SégolèneLazarus <seg.laz@ik.me>
2023-03-09 21:50:12 +0100

    exo3

--- imac_algoprog_2-master/TP1/exo3.cpp
+++ imac_algoprog_2-master/TP1/exo3.cpp
@@ -1,27 +1,53 @@
-#include "tp1.h"
-#include <QApplication>
+// #include "tp1.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <random>
+#include <vector>
 #include <time.h>
 
 #define return_and_display(result) return _.store(result);
 
 
-int search(int value, Array& toSort, int size)
+int search(int value, int array[], int size)
 {
-    Context _("search", value, size); // do not care about this, it allow the display of call stack
+// un tableau 
+//est-ce que tel nombre est dans le tableau
+//si oui, à quelle position
 
-    // your code
-    // check the last cell and if it does not correspond
-    // then consider a smaller array when recalling search
+    //compteur arrêt argument répéter
+
+    if(size==0){
+        return -1;
+    }
+    if(array[size-1]==value){
+        return size-1;
+    }
+
+    return search(value, array, size - 1)  ;
 }
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);  // create a window manager
-    MainWindow::instruction_duration = 400;  // make a pause between instruction display
-    MainWindow* w = new SearchWindow(search); // create a window for this exercice
-    w->show(); // show exercice
+    srand((unsigned)time(NULL));
+    const int size = 5;
+    int valTest = rand()%size;
+    int tab[size];
 
-    return a.exec(); // main loop while window is opened
+    std::cout << "valeur de test : " << valTest << "\n";
+
+    for(int i=0;i<size;i++){
+        tab[i]=rand()%10;
+    }
+
+    for(int i=0;i<size;i++){
+        std::cout << tab[i] << "\n";
+    }
+
+    std::cout << "séparation" << "\n";
+
+    std::cout << search(valTest,tab,size) << "\n";
 }
 
 
