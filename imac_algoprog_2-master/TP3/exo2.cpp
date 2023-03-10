097ff31fca21edc8e1058f1bec525411399303d3
LAZARUS SEGOLENE <segolene.lazarus@pccop1b101-14.ETUD>
2023-03-10 16:38:06 +0100

    exo2 presque fini youpi

--- imac_algoprog_2-master/TP3/exo2.cpp
+++ imac_algoprog_2-master/TP3/exo2.cpp
@@ -10,13 +10,50 @@ using std::size_t;
 
 const int tailleTb = 8;
 
-void binarySearchAll(int array[], int toSearch, int indexMin, int indexMax)
+int binarySearch(int array[], int toSearch)
 {
-	// do not use increments, use two different binary search loop
-    indexMin = indexMax = -1;
+
+	int start = 0;
+	int end = tailleTb;
+	int foundIndex = 0;
+
+	for(start;start<end;start++){
+		int mid = (start+end)/2;
+		if(toSearch>array[mid]){
+			start = mid +1;
+		}
+		else if(toSearch<array[mid]){
+			end = mid;
+		}
+		else{
+			foundIndex = mid;
+		}
+	}
+
+	return foundIndex;
+}
+
+void binarySearchAll(int array[], int toSearch, int* indexMin, int* indexMax)
+{
+
+	int start = 0;
+	int end = tailleTb;
+
+	indexMin = indexMax = binarySearch(array,toSearch);
+
+	//on cherche un intervalle
+
+	while(indexMax<end && array[indexMax+1]==toSearch){
+		indexMax++;
+	}
+
+	while(indexMin>=start && array[indexMin-1]==toSearch){
+		indexMin++;
+	}
+
 }
 
 int main(int argc, char *argv[])
 {
-	:( ouin je sais pattstt
+	
 }
