38c1ea8ee98511d5dda98a68b50cb90b6a3ce443
LAZARUS SEGOLENE <segolene.lazarus@pccop1b101-08.ETUD>
2023-03-10 11:48:26 +0100

    exo1

--- imac_algoprog_2-master/TP3/exo1.cpp
+++ imac_algoprog_2-master/TP3/exo1.cpp
@@ -1,21 +1,87 @@
-#include "tp3.h"
-#include <QApplication>
+// #include "tp1.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
-MainWindow* w = nullptr;
+#include <random>
+#include <vector>
+#include <time.h>
 using std::size_t;
 
-int binarySearch(Array& array, int toSearch)
+const int tailleTb = 8;
+
+int binarySearch(int array[], int toSearch)
 {
-	return -1;
+
+	int start = 0;
+	int end = tailleTb;
+	int foundIndex = 0;
+
+	for(start;start<end;start++){
+		int mid = (start+end)/2;
+		if(toSearch>array[mid]){
+			start = mid +1;
+		}
+		else if(toSearch<array[mid]){
+			end = mid;
+		}
+		else{
+			foundIndex = mid;
+		}
+	}
+
+	return foundIndex;
+}
+
+void selectionSort(int toSort[]){
+    bool comtuveu = true;
+    while(comtuveu){
+        comtuveu = false;
+        for(int i=0;i<tailleTb;i++){
+            for(int k=i;k<tailleTb;k++){
+                if(toSort[i] > toSort[k]){
+                    int n = toSort[i];
+                    toSort[i] = toSort[k];
+                    toSort[k] = n;
+                    k=tailleTb;
+                    comtuveu = true;
+                }
+            }
+        }
+    }
+	
 }
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	MainWindow::instruction_duration = 500;
-	w = new BinarySearchWindow(binarySearch);
-	w->show();
+	 srand((unsigned)time(NULL));
 
-	return a.exec();
+	int toSearch = rand()%20;
+
+	std::cout << "valeur à chercher :" << toSearch << "\n";
+
+	int tab[tailleTb];
+
+    for(int i=0;i<tailleTb;i++){
+        tab[i]=rand()%20;
+    }
+
+	std::cout << "tableau affichage" << "\n";
+
+    for(int i=0;i<tailleTb;i++){
+        std::cout << tab[i] << "\n";
+    }
+
+	selectionSort(tab);
+
+	std::cout << "tableau rangé" << "\n";
+
+	for(int i=0;i<tailleTb;i++){
+        std::cout << tab[i] << "\n";
+    }
+
+	std::cout << "resultat de la recherche" << "\n";
+
+	std::cout << binarySearch(tab,toSearch) << "\n";
+
 }
