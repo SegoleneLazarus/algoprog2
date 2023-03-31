7452c696f17e0a5156efc0ab253b7dc293ad30cd
LAZARUS SEGOLENE <segolene.lazarus@pccop1b101-08.ETUD>
2023-03-31 12:08:28 +0200

    exo1 FINITO PIPOOOOOOOOO

--- imac_algoprog_2-master/TP5/exo1.cpp
+++ imac_algoprog_2-master/TP5/exo1.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <time.h>
 #include <vector>
 #include <string>
@@ -11,37 +12,66 @@ std::vector<std::string> names(
     "Fanny", "Jeanne", "Elo"
 });
 
+std::vector<std::string> hash_table;
 
-int hash(std::vector<std::string> hash_table, std::string element)
+int hash( std::string element)
 {
-    return (int) element[0] % hash_table.size();
+    // element[0] prend la première lettre du mot
+    // size pr les vecteurs, length pour les string
+    int somme = 0;
+    for(int i=0;i<element.length();i++){
+        int val = (int) element[i] % hash_table.size();
+        somme += val;
+    }
+    return somme% hash_table.size();
 }
-
-void insert(std::vector<std::string> hash_table, std::string element)
+ 
+void insert(std::vector<std::string>& hash_table, std::string element)
 {
     // use (*this)[i] or this->get(i) to get a value at index i
-    hash_table[hash(hash_table, element)] 
+    // on compare la valeur du hash à l'indice et on insert
+
+    hash_table[hash(element)] = element;
 }
 
-/**
- * @brief buildHashTable: fill the HashTable with given names
- * @param table table to fiil
- * @param names array of names to insert
- * @param namesCount size of names array
- */
-void buildHashTable(std::vector<std::string> hash_table, std::string* names, int namesCount)
-{
 
+void buildHashTable(std::vector<std::string>& hash_table, std::vector<std::string> names, int namesCount)
+{
+    for(int i=0;i<namesCount;i++){
+        hash_table[i]=names[i];
+    }
 }
 
-bool contains(std::vector<std::string> hash_table, std::string element)
-{
-    // Note: Do not use iteration (for, while, ...)
+bool contains(std::vector<std::string>& hash_table, std::string element)
+{    
+    if(hash_table[hash(element)]==element){
+        return true;
+    }
     return false;
 }
 
 
 int main(int argc, char *argv[])
 {
-    //table.resize(10);
+    hash_table.resize(31);
+    buildHashTable(hash_table,names,31);
+
+
+    std::cout << "le hash de matthieu est le suivant!!!! :  " <<  hash("Matthieu") << "\n";
+
+   for(int i=0;i<names.size();i++){
+       std::cout << names[i] << " ; " << "\n";
+   }
+
+    insert(hash_table,"Matthieu");
+
+     std::cout << "affiche hash tab mtn" << "\n";
+
+    for(int i=0;i<hash_table.size();i++){
+       std::cout << hash_table[i] << " ; " << "\n";
+   }
+
+     std::cout << "est-ce Matthieu????????? " << hash_table[27] << "\n";
+
+    std::cout << "le tableau contient matthieu ? " << contains(hash_table,"Matthieu") << "\n";
 }
