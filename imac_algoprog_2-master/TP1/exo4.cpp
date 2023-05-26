9c15868e9a3e5af473ce986ff62f84aef8787e93
SégolèneLazarus <seg.laz@ik.me>
2023-05-26 16:37:18 +0200

    exo4 tp1

--- imac_algoprog_2-master/TP1/exo4.cpp
+++ imac_algoprog_2-master/TP1/exo4.cpp
@@ -9,27 +9,39 @@
 
 #define return_and_display(result) return _.store(result);
 
-void allEvens(int evens[], int array[], int evenSize, int arraySize)
+int allEvens(int evens[], int array[], int evenSize, int arraySize)
 {
-    
-    // if(arraySize==0){
-    //     return -1;
-    // }
-
     // dans array, vérfier si un nombre est pair
     // pour cela : n%2 = 0
     // if(array[arraySize-1] % 2 == 0)
     // si c'est égal à 0, alors on remplit evens
     // insert(index,value)
     // return allEvens()
-    
 
-    return;
+
+    if(arraySize==0){
+        return evenSize;
+    }
+    else if (array[arraySize - 1] % 2 == 0) {
+            evens[evenSize] = array[arraySize - 1];
+            evenSize++;
+            return allEvens(evens, array, evenSize + 1, arraySize - 1);
+        }
+        
+    
+    else{
+        return allEvens(evens, array, evenSize, arraySize - 1);
+    }
+    
 }
 
 int main(int argc, char *argv[])
 {
-    
+    int evens[150];
+    int array[9] = { 1,2,3,4,5,6,7,8,9 };
+    std::cout << "résultat : " << allEvens(evens, array, 0, 9) << std::endl;
+
+    return 0;
 
 }
 
